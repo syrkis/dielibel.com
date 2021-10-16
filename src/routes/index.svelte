f21898959770ab5393cc3152a6dd5d0a04beeb24
Noah Syrkis <noah@syrkis.com>
2021-10-16 14:35:59 +0200

    die libel

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,7 +2,24 @@
     import Tiles from '../comps/tiles.svelte';
 </script>
 
-<div>
-    <Tiles />
+<div class='wrapper'>
+    <div id="banner">
+        <p>Die Libel | The, Bart, The | Die, Bart, Die </p>
+    </div>
+    <div id="tiles">
+        <Tiles />
+    </div>
 </div>
 
+<style>
+    #tiles {
+    }
+    #banner {
+        position: absolute;
+        float: left;
+        left: 0;
+    }
+    #banner p {
+        transform: rotate(-90deg);
+    }
+</style>
