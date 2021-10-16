6a16f9811e1aa0eac28a4659452503c8d66e1254
Noah Syrkis <noah@syrkis.com>
2021-10-16 13:28:35 +0200

    die libel

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,22 @@
-<div>
+<div class='wrapper'>
+    <aside class="rotate">
+        Die Libel | The Libel | The, Bart, The | Noah Syrkis | Folke Sverdrup Saenger
+    </aside>
+
     <slot />
-    <div>
-        footer
-    </div>
 </div>
+
+<style>
+    .wrapper {
+        position: relative;
+    }
+    aside {
+        transform: rotate(-90deg);
+        transform-origin: 0 0;
+    }
+    .rotate {
+        display: flex;
+        align-items: bottom;
+        justify-content: bottom ;
+    }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,40 +1,8 @@
 <script>
-    import Definition from '../comps/definition.svelte';
-    import Newsletter from '../comps/newsletter.svelte';
+    import Tiles from '../comps/tiles.svelte';
 </script>
 
 <div>
-    <div class='grid'>
-        <div class='grid-item'><Definition /></div>
-        <div class="grid-item" id="red-square"></div>
-        <div class="grid-item"><Newsletter /></div>
-	<a href="/dialogues"><div class="grid-item"><h2>DIALOGUES</h2></div></a>
-        <div class="grid-item">5</div>
-        <div class="grid-item">6</div>
-    </div>
+    <Tiles />
 </div>
 
-<style>
-    #red-square {
-        background: #da3527;
-        width: 100%;
-    }
-    .grid {
-        display: grid;
-        grid-template-columns: repeat(auto-fill, minmax(25rem, 1fr));
-        grid-auto-rows: 1fr;
-        padding: 10px;
-    }
-    .grid::before {
-        content: '';
-        width: 0;
-        padding-bottom: 100%;
-        grid-row: 1/1;
-        grid-column: 1/1;
-    }
-    .grid > *:first-child {
-        grid-row: 1 / 1;
-        grid-column: 1 / 1;
-        font-align: center;
-    }
-</style>
