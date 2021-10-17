1b0e668bed143bde4850ef10a6e116ab24e3cb1a
Noah Syrkis <noah@syrkis.com>
2021-10-17 10:09:16 +0200

    die libel

---
+++ src/comps/tiles/definition.svelte
@@ -0,0 +1,43 @@
+<div class='wrapper'>
+    <div class="element">
+        <div class="header">
+            <h1>
+                DIE LIBEL.
+            </h1>
+        </div>
+        <div>
+            <ul>
+                <li>
+                    <i>a written or oral defamatory statement or representation that conveys an unjustly unfavorable impression</i>
+                </li>
+                <li>
+                    archaic : <i>a handbill especially attacking or defaming someone</i>
+                </li>
+            </ul>
+            <div class='cite'>
+                - Merriam-Webster
+            </div>
+        </div>
+    </div>
+</div>
+
+<style>
+    .cite { text-align: right; }
+    ul{ list-style:none; }
+    li { padding: 10px; text-indent: 2em; text-align: justify; hyphens: auto; }
+    .header {
+        font-style: italic;
+    }
+    .element {
+        width: 85%;
+        top: 50%;
+        left: 50%;
+        margin-right: -50%;
+        transform: translate(-50%, -50%);
+        position: absolute;
+    }
+    .wrapper{
+        width: 100%;
+        height: 100%;
+    }
+</style>
---
+++ src/comps/tiles/newsletter.svelte
@@ -0,0 +1,23 @@
+<div class='wrapper'>
+    <div class="element">
+        <h2>
+            NEWSLETTER
+        </h2>
+        <p>(click to signup)</p>
+    </div>
+</div>
+
+<style>
+    .element {
+        text-align: center;
+        top: 50%;
+        left: 50%;
+        margin-right: -50%;
+        transform: translate(-50%, -50%);
+        position: absolute;
+    }
+    .wrapper{
+        height: 100%;
+        font-size: 1.4rem;
+    }
+</style>
